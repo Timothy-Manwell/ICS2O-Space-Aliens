a68fe14f797b3529066087b98aaf565d83581cd0
Timothy-Manwell <Timothy-Manwell@users.noreply.github.com>
2022-11-08 19:42:29 +0000

    Prettified Code!

--- js/game.js
+++ js/game.js
@@ -6,15 +6,15 @@
 //This is the Phaser3 game configuration file
 
 /**
-* Start Phaser Game
-*/
+ * Start Phaser Game
+ */
 const config = {
   type: Phaser.Auto,
   width: 1920,
   height: 1080,
   // set background colour
   backgroundColor: 0x5f6e7a,
-}
+};
 
-const game = new Phaser.Game(config)
-console.log(game)
+const game = new Phaser.Game(config);
+console.log(game);
