a0a40c7e4b5f8539d6db91eaf5b4b6a82d904ee9
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-12-07 13:54:48 -0500

    gameScene laser and missile movement

--- js/gameScene.js
+++ js/gameScene.js
@@ -28,9 +28,13 @@
     preload() {
       console.log("Game Scene")
 
+      //images
       this.load.image("starBackground", "./assets/starBackground.png")
       this.load.image("ship", "./assets/spaceShip.png")
       this.load.image("missile", "./assets/missile.png")
+
+      //audio
+      this.load.audio("laser", "./assets/laser1.wav")
     }
   
     create(data) {
@@ -64,12 +68,20 @@
           this.fireMissile = true
           const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, "missile")
           this.missileGroup.add(aNewMissile)
+          this.sound.play("laser")
         }
       }
 
       if (keySpaceObj.isUp === true) {
         this.fireMissile = false
       }
+
+      this.missileGroup.children.each(function (item) {
+        item.y = item.y - 15
+        if (item.y < 0) {
+          item.destroy()
+        }
+      })
     }
 }
   export default GameScene
