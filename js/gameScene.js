21089a93382bb4ef7bc19ec80406a812f69a91c2
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-12-09 13:58:10 -0500

    alien fix 1

--- js/gameScene.js
+++ js/gameScene.js
@@ -13,7 +13,7 @@
     createAlien () {
       const alienXLocation = Math.floor(Math.random() * 1920) + 1
       let alienXVelocity = Math.floor(Math.random() * 50) + 1
-      alienVelocity *= Math.round(Math.random()) ? 1 : -1
+      alienXVelocity *= Math.round(Math.random()) ? 1 : -1
       const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
       anAlien.body.velocity.y = 200
       anAlien.body.velocity.x = alienXVelocity
